6e8a227862eca48208370fe2020fa577f5c3af25
Lucas Koerner <lcskoerner@gmail.com>
2020-10-25 22:23:53 -0400

    gif list as function component

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -1,17 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Gif from './gif';
 
-class GifList extends Component {
-  render() {
-    const { gifs } = this.props;
-    return (
-      <div className="gif-list">
-        {gifs.map((gif) => {
-          return <Gif id={gif.id} key={gif.id} updateGif={this.props.updateGif} />;
-        })}
-      </div>
-    );
-  }
-}
+const GifList = ({ gifs, updateGif }) => {
+  return (
+    <div className="gif-list">
+      {gifs.map(gif => <Gif id={gif.id} key={gif.id} updateGif={updateGif} />)}
+    </div>
+  );
+};
 
 export default GifList;
